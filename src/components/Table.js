d910a373b11bbf9c47177dd21a5b300ee1020e2e
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-08-05 07:25:21 -0300

    Conseguindo criar uma tabela referente ao 6 requisito.

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,7 +3,17 @@ import React, { Component } from 'react';
 export default class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <table>
+        <th value="Descrição">Descrição</th>
+        <th value="Tag">Tag</th>
+        <th value="Método de pagamento">Método de pagamento</th>
+        <th value="Valor">Valor</th>
+        <th value="Moeda">Moeda</th>
+        <th value="Câmbio utilizado">Câmbio utilizado</th>
+        <th value="Valor convertido">Valor convertido</th>
+        <th value="Moeda de conversão">Moeda de conversão</th>
+        <th value="Editar/Excluir">Editar/Excluir</th>
+      </table>
     );
   }
 }
